001ddc93d8ab13b11c9d6ba6a819c08a4fadfaa4
ArielYosefMR <105983606+ArielYosefMR@users.noreply.github.com>
2023-11-30 11:23:49 -0600

    Update urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from project_name import views
+from buscadorRI import views
 
 urlpatterns = [
     path('', views.buscar),
